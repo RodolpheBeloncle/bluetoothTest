0fc722065f381ab55325e92143f958d43e9457dc
RodolpheBeloncle <rbeloncle@gmail.com>
2024-06-08 12:09:02 +0200

    fix basic websocket

--- src/app/pages/messages/messages.page.ts
+++ src/app/pages/messages/messages.page.ts
@@ -1,140 +1,173 @@
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { AuthService } from './../../services/auth.service';
+// import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+// import { ActivatedRoute } from '@angular/router';
+// import { AuthService } from './../../services/auth.service';
+// import { WebSocketService } from '../../services/web-socket.service';
+// import { DataService } from './../../services/data.service';
+// import { Subscription } from 'rxjs';
+// import { Message, Group } from '../../types/data.service.types';
+// import { ToastController, LoadingController } from '@ionic/angular';
+
+// @Component({
+//   selector: 'app-messages',
+//   templateUrl: './messages.page.html',
+//   styleUrls: ['./messages.page.scss'],
+// })
+// export class MessagesPage implements OnInit, OnDestroy {
+//   @ViewChild('content', { static: false }) content!: ElementRef;
+//   messages: Message[] = [];
+//   group: Group | undefined | null;
+//   messageText: string = '';
+//   currentUserId: string = '';
+//   private groupId: string | null = null;
+//   private messagesSubscription!: Subscription;
+//   private loading: HTMLIonLoadingElement | null = null;
+
+//   constructor(
+//     private authService: AuthService,
+//     private route: ActivatedRoute,
+//     private dataService: DataService,
+//     private webSocketService: WebSocketService,
+//     private toastController: ToastController,
+//     private loadingController: LoadingController
+//   ) { }
+
+//   async ngOnInit() {
+//     this.groupId = this.route.snapshot.paramMap.get('groupid');
+//     this.loading = await this.loadingController.create({
+//       message: 'Loading messages...',
+//     });
+//     await this.loading.present();
+
+//     this.authService.getCurrentUser().subscribe(async user => {
+//       if (user) {
+//         this.currentUserId = user.id;
+//         if (this.groupId) {
+//           await this.loadGroup(this.groupId);
+//           this.loadMessages(this.groupId);
+//           this.joinGroup(this.groupId);
+//         }
+//       }
+//       if (this.loading) {
+//         await this.loading.dismiss();
+//         this.loading = null;
+//       }
+//     });
+
+//     this.messagesSubscription = this.webSocketService.messages$.subscribe((newMessages: any) => {
+//       console.log('New messages received:', newMessages);
+//       if (Array.isArray(newMessages)) {
+//         this.messages = [...this.messages, ...newMessages];
+//       } else {
+//         this.messages = [...this.messages, newMessages];
+//       }
+//       this.scrollToBottom();
+//     });
+
+//     this.webSocketService.initWebSocket();
+//   }
+
+//   ngOnDestroy() {
+//     if (this.groupId) {
+//       this.webSocketService.leaveGroup(this.currentUserId, this.groupId);
+//     }
+//     if (this.messagesSubscription) {
+//       this.messagesSubscription.unsubscribe();
+//     }
+//     this.webSocketService.closeWebSocket();
+//   }
+
+//   async loadGroup(groupId: string) {
+//     try {
+//       this.group = await this.dataService.getGroupById(parseInt(groupId, 10)).toPromise();
+//     } catch (error) {
+//       console.error('Error loading group:', error);
+//       await this.showToast('Error loading group');
+//       this.group = null; // Assign null if group is not found
+//     }
+//   }
+
+//   loadMessages(groupId: string) {
+//     this.dataService.getGroupMessages(parseInt(groupId, 10)).subscribe(messages => {
+//       this.messages = messages;
+//       this.scrollToBottom();
+//     });
+//   }
+
+//   joinGroup(groupId: string) {
+//     this.webSocketService.joinGroup(this.currentUserId, groupId);
+//     this.showToast('Joined group successfully');
+//   }
+
+//   sendMessage() {
+//     if (this.messageText.trim() && this.groupId) {
+//       const message: Message = {
+//         userId: this.currentUserId,
+//         groupId: this.groupId,
+//         text: this.messageText,
+//         action: 'message',
+//         type: 'text', // Assuming type is 'text'
+//         created_at: new Date()
+//       };
+//       this.webSocketService.sendMessage(message);
+//       this.messages.push(message); // Immediately add the message to the array
+//       this.messageText = '';
+//       this.scrollToBottom();
+//       console.log('Message sent', this.currentUserId, this.groupId, this.messageText);
+//       this.showToast('Message sent');
+//     } else {
+//       console.error('Message text is empty. Cannot send message.');
+//     }
+//   }
+
+//   isCurrentUserMessage(message: Message): boolean {
+//     return message.userId === this.currentUserId;
+//   }
+
+//   async showToast(message: string) {
+//     const toast = await this.toastController.create({
+//       message,
+//       duration: 2000,
+//       position: 'top',
+//     });
+//     toast.present();
+//   }
+
+//   scrollToBottom() {
+//     setTimeout(() => {
+//       this.content.nativeElement.scrollToBottom(300);
+//     }, 100);
+//   }
+// }
+
+
+import { Component, OnInit } from '@angular/core';
 import { WebSocketService } from '../../services/web-socket.service';
-import { DataService } from './../../services/data.service';
-import { Subscription } from 'rxjs';
-import { Message, Group } from '../../types/data.service.types';
-import { ToastController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.page.html',
   styleUrls: ['./messages.page.scss'],
 })
-export class MessagesPage implements OnInit, OnDestroy {
-  @ViewChild('content', { static: false }) content!: ElementRef;
-  messages: Message[] = [];
-  group: Group | undefined | null;
-  messageText: string = '';
-  currentUserId: string = '';
-  private groupId: string | null = null;
-  private messagesSubscription!: Subscription;
-  private loading: HTMLIonLoadingElement | null = null;
+export class MessagesPage implements OnInit {
 
-  constructor(
-    private authService: AuthService,
-    private route: ActivatedRoute,
-    private dataService: DataService,
-    private webSocketService: WebSocketService,
-    private toastController: ToastController,
-    private loadingController: LoadingController
-  ) { }
+  receivedMessages: string[] = [];
 
-  async ngOnInit() {
-    this.groupId = this.route.snapshot.paramMap.get('groupid');
-    this.loading = await this.loadingController.create({
-      message: 'Loading messages...',
-    });
-    await this.loading.present();
+  constructor(private websocketService: WebSocketService) { }
 
-    this.authService.getCurrentUser().subscribe(async user => {
-      if (user) {
-        this.currentUserId = user.id;
-        if (this.groupId) {
-          await this.loadGroup(this.groupId);
-          this.loadMessages(this.groupId);
-          this.joinGroup(this.groupId);
-        }
-      }
-      if (this.loading) {
-        await this.loading.dismiss();
-        this.loading = null;
-      }
-    });
+  ngOnInit(): void {
+    this.websocketService.connect();
 
-    this.messagesSubscription = this.webSocketService.messages$.subscribe((newMessages: any) => {
-      console.log('New messages received:', newMessages);
-      if (Array.isArray(newMessages)) {
-        this.messages = [...this.messages, ...newMessages];
-      } else {
-        this.messages = [...this.messages, newMessages];
-      }
-      this.scrollToBottom();
-    });
+    this.websocketService.messageReceived.subscribe((message: string) => {
+      let streamedMessage = JSON.parse(message);
+      console.log('Message received:', streamedMessage.data.text);
+      console.log('type of:', typeof streamedMessage.data.text);
 
-    this.webSocketService.initWebSocket();
-  }
-
-  ngOnDestroy() {
-    if (this.groupId) {
-      this.webSocketService.leaveGroup(this.currentUserId, this.groupId);
-    }
-    if (this.messagesSubscription) {
-      this.messagesSubscription.unsubscribe();
-    }
-    this.webSocketService.closeWebSocket();
-  }
-
-  async loadGroup(groupId: string) {
-    try {
-      this.group = await this.dataService.getGroupById(parseInt(groupId, 10)).toPromise();
-    } catch (error) {
-      console.error('Error loading group:', error);
-      await this.showToast('Error loading group');
-      this.group = null; // Assign null if group is not found
-    }
-  }
-
-  loadMessages(groupId: string) {
-    this.dataService.getGroupMessages(parseInt(groupId, 10)).subscribe(messages => {
-      this.messages = messages;
-      this.scrollToBottom();
+      this.receivedMessages.push(streamedMessage.data.text);
     });
   }
 
-  joinGroup(groupId: string) {
-    this.webSocketService.joinGroup(this.currentUserId, groupId);
-    this.showToast('Joined group successfully');
-  }
-
-  sendMessage() {
-    if (this.messageText.trim() && this.groupId) {
-      const message: Message = {
-        userId: this.currentUserId,
-        groupId: this.groupId,
-        text: this.messageText,
-        action: 'message',
-        type: 'text', // Assuming type is 'text'
-        created_at: new Date()
-      };
-      this.webSocketService.sendMessage(message);
-      this.messages.push(message); // Immediately add the message to the array
-      this.messageText = '';
-      this.scrollToBottom();
-      console.log('Message sent', this.currentUserId, this.groupId, this.messageText);
-      this.showToast('Message sent');
-    } else {
-      console.error('Message text is empty. Cannot send message.');
-    }
-  }
-
-  isCurrentUserMessage(message: Message): boolean {
-    return message.userId === this.currentUserId;
-  }
-
-  async showToast(message: string) {
-    const toast = await this.toastController.create({
-      message,
-      duration: 2000,
-      position: 'top',
-    });
-    toast.present();
-  }
-
-  scrollToBottom() {
-    setTimeout(() => {
-      this.content.nativeElement.scrollToBottom(300);
-    }, 100);
+  sendMessage(): void {
+    const message = 'Hello, WebSocket!';
+    this.websocketService.sendMessage(message);
   }
 }
--- src/app/services/web-socket.service.ts
+++ src/app/services/web-socket.service.ts
@@ -1,49 +1,131 @@
+// import { Injectable } from '@angular/core';
+// import { BehaviorSubject } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class WebSocketService {
+//   private wsUrl = 'ws://localhost:3000/ws'; // Update to your WebSocket URL
+//   private socket!: WebSocket;
+//   private messagesSubject = new BehaviorSubject<any[]>([]);
+//   public messages$ = this.messagesSubject.asObservable();
+//   private isConnected: boolean = false;
+//   private reconnectAttempts: number = 0;
+//   private maxReconnectAttempts: number = 5;
+
+//   constructor() {
+//     this.initWebSocket();
+//   }
+
+//   initWebSocket() {
+//     console.log('Initializing WebSocket...');
+//     this.socket = new WebSocket(this.wsUrl);
+
+//     this.socket.onopen = () => {
+//       console.log('WebSocket connected');
+//       this.isConnected = true;
+//       this.reconnectAttempts = 0;
+//     };
+
+//     this.socket.onclose = () => {
+//       console.log('WebSocket disconnected');
+//       this.isConnected = false;
+//       if (this.reconnectAttempts < this.maxReconnectAttempts) {
+//         setTimeout(() => {
+//           this.reconnectAttempts++;
+//           this.initWebSocket();
+//         }, 2000);
+//       } else {
+//         console.error('Max reconnection attempts reached. Stopping further attempts.');
+//       }
+//     };
+
+//     this.socket.onmessage = (event) => {
+//       const message = JSON.parse(event.data);
+//       console.log('WebSocket message received:', message);
+//       this.messagesSubject.next([...this.messagesSubject.value, message]);
+//     };
+
+//     this.socket.onerror = (error) => {
+//       console.error('WebSocket error:', error);
+//     };
+//   }
+
+//   sendMessage(message: { userId: string; groupId: string; text: string; action: string }) {
+//     if (!this.isConnected) {
+//       console.error('WebSocket is not connected. Cannot send message.');
+//       return;
+//     }
+//     this.socket.send(JSON.stringify(message));
+//     console.log('Message sent:', message);
+//   }
+
+//   joinGroup(userId: string, groupId: string) {
+//     const joinMessage = { userId, groupId, text: '', action: 'join' };
+//     this.sendMessage(joinMessage);
+//   }
+
+//   leaveGroup(userId: string, groupId: string) {
+//     const leaveMessage = { userId, groupId, text: '', action: 'leave' };
+//     this.sendMessage(leaveMessage);
+//   }
+
+//   sendMessageToGroup(userId: string, groupId: string, text: string) {
+//     const message = { userId, groupId, text, action: 'message' };
+//     this.sendMessage(message);
+//   }
+
+//   getHistory(userId: string, groupId: string) {
+//     const historyMessage = { userId, groupId, text: '', action: 'history' };
+//     this.sendMessage(historyMessage);
+//   }
+
+//   clearMessages() {
+//     this.messagesSubject.next([]);
+//   }
+
+//   closeWebSocket() {
+//     if (this.socket) {
+//       this.socket.close();
+//     }
+//   }
+// }
+
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebSocketService {
-  private wsUrl = 'ws://localhost:3000/ws'; // Update to your WebSocket URL
   private socket!: WebSocket;
-  private messagesSubject = new BehaviorSubject<any[]>([]);
-  public messages$ = this.messagesSubject.asObservable();
-  private isConnected: boolean = false;
-  private reconnectAttempts: number = 0;
-  private maxReconnectAttempts: number = 5;
+  messageReceived: Subject<string> = new Subject<string>();
 
-  constructor() {
-    this.initWebSocket();
-  }
+  constructor() { }
 
-  initWebSocket() {
-    console.log('Initializing WebSocket...');
-    this.socket = new WebSocket(this.wsUrl);
+  connect(): void {
+    this.socket = new WebSocket('http://localhost:3000/ws');
 
     this.socket.onopen = () => {
-      console.log('WebSocket connected');
-      this.isConnected = true;
-      this.reconnectAttempts = 0;
-    };
-
-    this.socket.onclose = () => {
-      console.log('WebSocket disconnected');
-      this.isConnected = false;
-      if (this.reconnectAttempts < this.maxReconnectAttempts) {
-        setTimeout(() => {
-          this.reconnectAttempts++;
-          this.initWebSocket();
-        }, 2000);
-      } else {
-        console.error('Max reconnection attempts reached. Stopping further attempts.');
-      }
+      console.log('WebSocket connection established.');
     };
 
     this.socket.onmessage = (event) => {
-      const message = JSON.parse(event.data);
-      console.log('WebSocket message received:', message);
-      this.messagesSubject.next([...this.messagesSubject.value, message]);
+      const message = event.data;
+      console.log('Received message:', message);
+      this.messageReceived.next(message);
+    };
+
+
+    this.socket.onclose = (event) => {
+      console.log('WebSocket connection closed:', event);
+
+      // if connextion is closed, try to reconnect
+      setTimeout(() => {
+        this.reconnect();
+      }, 1000);
+
+
     };
 
     this.socket.onerror = (error) => {
@@ -51,42 +133,23 @@ export class WebSocketService {
     };
   }
 
-  sendMessage(message: { userId: string; groupId: string; text: string; action: string }) {
-    if (!this.isConnected) {
-      console.error('WebSocket is not connected. Cannot send message.');
-      return;
-    }
-    this.socket.send(JSON.stringify(message));
-    console.log('Message sent:', message);
+
+  sendMessage(message: string): void {
+    // stringify the message and send it
+    this.socket.send(JSON.stringify({
+      action: "message",
+      userId: 5,
+      groupId: 1,
+      text: message
+    })
+    );
   }
 
-  joinGroup(userId: string, groupId: string) {
-    const joinMessage = { userId, groupId, text: '', action: 'join' };
-    this.sendMessage(joinMessage);
+  reconnect(): void {
+    this.socket = new WebSocket('http://localhost:3000/ws');
   }
 
-  leaveGroup(userId: string, groupId: string) {
-    const leaveMessage = { userId, groupId, text: '', action: 'leave' };
-    this.sendMessage(leaveMessage);
-  }
-
-  sendMessageToGroup(userId: string, groupId: string, text: string) {
-    const message = { userId, groupId, text, action: 'message' };
-    this.sendMessage(message);
-  }
-
-  getHistory(userId: string, groupId: string) {
-    const historyMessage = { userId, groupId, text: '', action: 'history' };
-    this.sendMessage(historyMessage);
-  }
-
-  clearMessages() {
-    this.messagesSubject.next([]);
-  }
-
-  closeWebSocket() {
-    if (this.socket) {
-      this.socket.close();
-    }
+  closeConnection(): void {
+    this.socket.close();
   }
 }
